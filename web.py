c0036371dbce2f459c682e7f2661c549b8fe55e2
yehoshua <yehoshua.cohen.91@gmail.com>
2020-02-29 12:42:03 +0200

    setup

--- web.py
+++ web.py
@@ -1,5 +1,28 @@
 from bs4 import BeautifulSoup
-import  requests
+import requests
+import urllib.request
+import time
 
-with open('simple.html') as html_file:
-    soup =  BeautifulSoup(html_file, 'lxml')
+URL = 'https://www.numbeo.com/cost-of-living/'
+
+
+def get_data_from_URL(url):
+    return requests.get(url)
+
+
+def parse_response(response):
+    return BeautifulSoup(response.text, 'html.parser')
+
+
+def manage_public_welfare():
+    response = get_data_from_URL(URL)
+    data = parse_response(response)
+    print(data)
+
+
+def main():
+    manage_public_welfare()
+
+
+if __name__ == '__main__':
+    main()
