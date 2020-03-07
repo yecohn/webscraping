0412285adf88e0edd7aafed0088b9a8a0d6a49a5
Yaniv Cohen <yaniv92648@gmail.com>
2020-03-07 11:33:46 +0200

    Fixed problem

--- web.py
+++ web.py
@@ -1,23 +1,27 @@
 from bs4 import BeautifulSoup
 import requests
-import urllib
+import urllib.request
 import time
 
 URL = 'https://www.numbeo.com/cost-of-living/'
 
 
 def get_data_from_URL(url):
-    return requests.get(URL)
+    return requests.get(url)
 
 
 def parse_response(response):
-    return BeautifulSoup(response.text, 'data.html')
+    return BeautifulSoup(response.text, 'html.parser')
 
 
-def main():
+def manage_public_welfare():
     response = get_data_from_URL(URL)
     data = parse_response(response)
     print(data)
+
+
+def main():
+    manage_public_welfare()
 
 
 if __name__ == '__main__':
