25d66c527a771491c0c8543ef6a839364c82ae25
Yaniv Cohen <yaniv92648@gmail.com>
2020-03-07 08:31:59 +0200

    setup

--- web_tuto.py
+++ web_tuto.py
@@ -1,5 +1,23 @@
 from bs4 import BeautifulSoup
-import  requests
+import requests
+import urllib
+import time
 
-with open('simple.html') as html_file:
-    soup =  BeautifulSoup(html_file, 'lxml')
+URL = 'https://www.numbeo.com/cost-of-living/'
+
+
+def get_data_from_URL(url):
+    return requests.get(URL)
+
+
+def parse_response(response):
+    return BeautifulSoup(response.text, 'html.parser')
+
+
+def main():
+    response = get_data_from_URL(URL)
+    data = parse_response(response)
+
+
+if __name__ == '__main__':
+    main()
