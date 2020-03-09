67c5c1acc916deacc3dcd76fff0a983381fbcab5
Yaniv Cohen <yaniv92648@gmail.com>
2020-03-09 17:39:42 +0200

    added support in web main file

--- web.py
+++ web.py
@@ -1,27 +1,44 @@
 from bs4 import BeautifulSoup
 import requests
-import urllib.request
-import time
-
-URL = 'https://www.numbeo.com/cost-of-living/'
+import re
+from enum import Enum
 
 
-def get_data_from_URL(url):
-    return requests.get(url)
+class HAPPINESS(Enum):
+    COST_OF_LIVING = 1
+    CRIME = 3
+    CLIMATE = 4
+    FOOD_PRICES = 5
+    GAS_PRICES = 6
+    HEALTH_CARE = 7
+    POLLUTION = 8
+    PROPERTY_INVESTMENT = 9
+    QUALITY_OF_LIFE = 10
+    TAXI_FARE = 11
+    TRAFFIC = 12
 
 
-def parse_response(response):
-    return BeautifulSoup(response.text, 'html.parser')
+MAIN_URL = 'https://www.numbeo.com/cost-of-living/'
 
 
-def manage_public_welfare():
-    response = get_data_from_URL(URL)
-    data = parse_response(response)
-    print(data)
+def get_raw_data():
+    return requests.get(MAIN_URL).text
+
+
+def get_all_urls(raw_data):
+    soup = BeautifulSoup(raw_data, 'html.parser')
+    return [element.get('value') for element in soup.findAll('option', attrs={"value": re.compile('https.+')})]
+
+
+def navigate_to_subject_according_to_user():
+    raw_data = get_raw_data()
+    main_urls = get_all_urls(raw_data)
+
+    print(main_urls)
 
 
 def main():
-    manage_public_welfare()
+    navigate_to_subject_according_to_user()
 
 
 if __name__ == '__main__':
