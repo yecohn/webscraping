bebd28e10069d27fff8fdd2ef489145888011efb
yehoshua <yehoshua.cohen.91@gmail.com>
2020-03-20 19:43:45 +0200

    preparation for user interface.

---
+++ parser.py
@@ -0,0 +1,9 @@
+import welfare_data
+import welfare_html_parser
+import sys
+import argparse
+
+
+
+#TODO: discuss what features are relevant to be parse.
+#TODO: start to create database, to be continued tomorow
