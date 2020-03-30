d5ad330549b4f79af7f6efd93b0054343f230276
yehoshua <yehoshua.cohen.91@gmail.com>
2020-03-30 19:38:39 +0300

    final modif, without countries_name

--- db_building.py
+++ db_building.py
@@ -6,9 +6,9 @@ import datetime
 import mysql.connector
 from _collections import defaultdict
 DATABASE_NAME= 'parser'
-HOST = '<ENTER localhost>'
-USERNAME = '<ENTER root (or your username)>'
-PASSWD= '<ENTER your mysql password>'
+HOST = 'localhost'
+USERNAME = 'root'
+PASSWD= 'azerty051210'
 
 try:
     mydb = mysql.connector.connect(host=HOST,
@@ -56,7 +56,6 @@ for cont in list_country:
     dic_country[cont] = counter
     counter += 1
 
-print(list_country)
 
 
 
@@ -70,7 +69,6 @@ cur.execute('''CREATE TABLE  property_price(
                        id  INT PRIMARY KEY AUTO_INCREMENT,
                        country_id INT,
                        year INT,
-                       country VARCHAR(255),
                        price_to_income FLOAT,
                        gross_rental_inside_center FLOAT,
                        gross_rental_outside_center FLOAT,
@@ -89,24 +87,23 @@ for year in YEARS:
     for row in welfare_data.log_ranking(SUBJECT_NAME, year)[1:]:
         if row[0] in dic_country:
             print('the country: {} for year: {} is being added to table {}'.format(row[0], year,   SUBJECT_NAME))
-            fields = [dic_country[row[0]], year, *row]
+            fields = [dic_country[row[0]], year, *row[1:]]
         else:
             dic_country[row[0]] = counter
             counter += 1
-            fields = [dic_country[row[0]], year, *row]
+            fields = [dic_country[row[0]], year, *row[1:]]
             cur.execute('INSERT INTO countries(country_name) VALUES (%s)', (row[0],))
             mydb.commit()
-        cur.execute("INSERT INTO property_price (country_id, year, country, price_to_income, gross_rental_inside_center, gross_rental_outside_center, \
+        cur.execute("INSERT INTO property_price (country_id, year, price_to_income, gross_rental_inside_center, gross_rental_outside_center, \
                            price_rent_city_center, \
                            price_rent_outside_city_center, mortgage_as_prc_income, affordability) VALUES \
-                           (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", fields)
+                           (%s, %s, %s, %s, %s, %s, %s, %s, %s)", fields)
 
 mydb.commit()
 cur.execute('''CREATE TABLE  cost_of_living(
                                id  INT PRIMARY KEY AUTO_INCREMENT,
                                country_id INT,
                                year INT,
-                               country VARCHAR(255),
                                cost_of_living FLOAT,
                                rent FLOAT,
                                cost_of_living_and_rent FLOAT,
@@ -122,23 +119,22 @@ for year in YEARS:
     for row in welfare_data.log_ranking(SUBJECT_NAME, year)[1:]:
         if row[0] in dic_country:
             print('the country: {} for year: {} is being added to table {}'.format(row[0], year,   SUBJECT_NAME))
-            fields = [dic_country[row[0]], year, *row]
+            fields = [dic_country[row[0]], year, *row[1:]]
         else:
             dic_country[row[0]] = counter
             counter += 1
-            fields = [dic_country[row[0]], year, *row]
+            fields = [dic_country[row[0]], year, *row[1:]]
             cur.execute('INSERT INTO countries(country_name) VALUES (%s)', (row[0],))
             mydb.commit()
-        cur.execute("INSERT INTO cost_of_living (country_id, year, country, cost_of_living, rent, cost_of_living_and_rent, \
+        cur.execute("INSERT INTO cost_of_living (country_id, year, cost_of_living, rent, cost_of_living_and_rent, \
                                    groceries_prices, restaurant_prices, local_purchasing_power) VALUES \
-                                   (%s, %s, %s, %s, %s, %s, %s, %s, %s)", fields)
+                                   (%s, %s, %s, %s, %s, %s, %s, %s)", fields)
 mydb.commit()
 
 cur.execute('''CREATE TABLE  traffic(
                                id  INT PRIMARY KEY AUTO_INCREMENT,
                                country_id INT,
                                year INT,
-                               country VARCHAR(255),
                                traffic_id FLOAT,
                                time_id FLOAT,
                                time_exp_id FLOAT,
@@ -153,16 +149,16 @@ for year in YEARS:
     for row in welfare_data.log_ranking(SUBJECT_NAME, year)[1:]:
         if row[0] in dic_country:
             print('the country: {} for year: {} is being added to table {}'.format(row[0], year,   SUBJECT_NAME))
-            fields = [dic_country[row[0]], year, *row]
+            fields = [dic_country[row[0]], year, *row[1:]]
         else:
             dic_country[row[0]] = counter
             counter += 1
-            fields = [dic_country[row[0]], year, *row]
+            fields = [dic_country[row[0]], year, *row[1]]
             cur.execute('INSERT INTO countries(country_name) VALUES (%s)', (row[0],))
             mydb.commit()
-        cur.execute("INSERT INTO traffic (country_id, year, country, traffic_id, time_id, time_exp_id, inefficiency_id , \
+        cur.execute("INSERT INTO traffic (country_id, year, traffic_id, time_id, time_exp_id, inefficiency_id , \
                                    CO2_emission_id) VALUES \
-                                   (%s, %s, %s, %s, %s, %s, %s, %s)", fields)
+                                   (%s, %s, %s, %s, %s, %s, %s)", fields)
 
 mydb.commit()
 
@@ -170,7 +166,6 @@ cur.execute('''CREATE TABLE  health_care(
                                 id  INT PRIMARY KEY AUTO_INCREMENT, 
                                 country_id INT,
                                 year INT,  
-                                country VARCHAR(255),
                                 health_care_id FLOAT,
                                 health_care_exp_id FLOAT,
                                 foreign key (country_id) references countries(country_id)
@@ -182,15 +177,15 @@ for year in YEARS:
     for row in welfare_data.log_ranking(SUBJECT_NAME, year)[1:]:
         if row[0] in dic_country:
             print('the country: {} for year: {} is being added to table {}'.format(row[0], year,   SUBJECT_NAME))
-            fields = [dic_country[row[0]], year, *row]
+            fields = [dic_country[row[0]], year, *row[1:]]
         else:
             dic_country[row[0]] = counter
             counter += 1
-            fields = [dic_country[row[0]], year, *row]
+            fields = [dic_country[row[0]], year, *row[1:]]
             cur.execute('INSERT INTO countries(country_name) VALUES (%s)', (row[0],))
             mydb.commit()
-        cur.execute("INSERT INTO health_care (country_id, year, country, health_care_id, health_care_exp_id) VALUES \
-                                    (%s,%s, %s, %s, %s)", fields)
+        cur.execute("INSERT INTO health_care (country_id, year,health_care_id, health_care_exp_id) VALUES \
+                                    (%s,%s, %s, %s)", fields)
 
 
 mydb.commit()
@@ -199,7 +194,6 @@ cur.execute('''CREATE TABLE  pollution(
                                 id  INT PRIMARY KEY AUTO_INCREMENT, 
                                 country_id INT,
                                 year INT,
-                                country VARCHAR(255),
                                 pollution_id INT,
                                 exp_pollution_id INT,
                                 foreign key (country_id) references countries(country_id)
@@ -211,15 +205,15 @@ for year in YEARS:
     for row in welfare_data.log_ranking(SUBJECT_NAME, year)[1:]:
         if row[0] in dic_country:
             print('the country: {} for year: {} is being added to table {}'.format(row[0], year,   SUBJECT_NAME))
-            fields = [dic_country[row[0]], year, *row]
+            fields = [dic_country[row[0]], year, *row[1:]]
         else:
             dic_country[row[0]] = counter
             counter += 1
-            fields = [dic_country[row[0]], year, *row]
+            fields = [dic_country[row[0]], year, *row[1:]]
             cur.execute('INSERT INTO countries(country_name) VALUES (%s)', (row[0],))
             mydb.commit()
-        cur.execute("INSERT INTO pollution (country_id, year, country, pollution_id, exp_pollution_id) VALUES \
-                                    (%s ,%s, %s, %s, %s)", fields)
+        cur.execute("INSERT INTO pollution (country_id, year, pollution_id, exp_pollution_id) VALUES \
+                                    (%s ,%s, %s, %s)", fields)
 
 mydb.commit()
 
@@ -228,7 +222,6 @@ cur.execute('''CREATE TABLE  quality_life(
                                 id  INT PRIMARY KEY AUTO_INCREMENT, 
                                 country_id INT, 
                                 year INT,
-                                country VARCHAR(255),
                                 quality_life_id FLOAT,
                                 purchase_power_id FLOAT,
                                 safety_id FLOAT,
@@ -247,18 +240,18 @@ for year in YEARS:
     for row in welfare_data.log_ranking(SUBJECT_NAME, year)[1:]:
         if row[0] in dic_country:
             print('the country: {} for year: {} is being added to table {}'.format(row[0], year,   SUBJECT_NAME))
-            fields = [dic_country[row[0]], year, *row]
+            fields = [dic_country[row[0]], year, *row[1:]]
         else:
             dic_country[row[0]] = counter
             counter += 1
-            fields = [dic_country[row[0]], year, *row]
+            fields = [dic_country[row[0]], year, *row[1:]]
             cur.execute('INSERT INTO countries(country_name) VALUES (%s)', (row[0],))
             mydb.commit()
 
-        cur.execute("INSERT INTO quality_life (country_id, year, country, quality_life_id, purchase_power_id, safety_id, \
+        cur.execute("INSERT INTO quality_life (country_id, year, quality_life_id, purchase_power_id, safety_id, \
                                     health_care_id, cost_living_id, property_price_to_income_id, \
                                     traffic_commute_time_id, pollution_id, climate_id) VALUES \
-                                     (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", fields)
+                                     (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", fields)
 
 
 mydb.commit()
@@ -267,7 +260,6 @@ cur.execute('''CREATE TABLE  crime(
                                 id  INT PRIMARY KEY AUTO_INCREMENT, 
                                 country_id INT, 
                                 year INT,
-                                country VARCHAR(255),
                                 crime_id FLOAT,
                                 safety_id FLOAT,
                                 foreign key (country_id) references countries(country_id)
@@ -280,17 +272,17 @@ for year in YEARS:
     for row in welfare_data.log_ranking(SUBJECT_NAME, year)[1:]:
         if row[0] in dic_country:
             print('the country: {} for year: {} is being added to table {}'.format(row[0], year, SUBJECT_NAME))
-            fields = [dic_country[row[0]], year, *row]
+            fields = [dic_country[row[0]], year, *row[1:]]
         else:
             dic_country[row[0]] = counter
             counter += 1
-            fields = [dic_country[row[0]], year, *row]
+            fields = [dic_country[row[0]], year, *row[1:]]
             cur.execute('INSERT INTO countries(country_name) VALUES (%s)', (row[0],))
             mydb.commit()
-        cur.execute("INSERT INTO crime (country_id, year, country, crime_id, safety_id) VALUES \
-                                    (%s, %s, %s, %s, %s )", fields)
+        cur.execute("INSERT INTO crime (country_id, year, crime_id, safety_id) VALUES \
+                                    (%s, %s, %s, %s )", fields)
 
-
+mydb.commit()
 
 
 
