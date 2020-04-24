7756390227c591133f5cdf47320c5f32f36f3ded
Yaniv Cohen <yaniv92648@gmail.com>
2020-04-24 15:24:14 +0300

    Fixed password

--- config.py
+++ config.py
@@ -64,7 +64,7 @@ INVALID_PARAMS = 'You have entered invalid parameters\n' + HELP_DESC
 DATABASE_NAME = 'welfare'
 HOST = 'localhost'
 USERNAME = 'root'
-PASSWORD = 'Yc7350328'
+PASSWORD = 'azerty051210'
 
 
 def exit_program():
